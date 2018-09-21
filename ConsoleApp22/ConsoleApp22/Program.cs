8fa93782a35870077f32fb6fc69b58b609a93bdd
SHLOK BAWA ITD-18025 <43333337+SHLOKBAWA@users.noreply.github.com>
2018-09-21 10:41:58 +0530

    Add files via upload

---
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 30, y = 80, z = 90;
+            double sum = x + y + z;
+            double average = sum / 3;
+            Console.WriteLine("Average of " + x+" , " + y+" and " + z +" is " + average);
+        }
+    }
+}
